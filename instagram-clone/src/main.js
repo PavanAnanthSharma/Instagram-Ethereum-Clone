e48023f97d4068d25a43c0503a4b4a51fec10635
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-06-20 13:33:52 +0530

    Update main.js

--- instagram-clone/src/main.js
+++ instagram-clone/src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+import Vue from 'vue'; 
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
