cf2aec34da91977e43057714fb9537ca1fcda7d7
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-06-20 13:33:59 +0530

    Update main.js

--- instagram-clone/src/main.js
+++ instagram-clone/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'; 
-import BootstrapVue from 'bootstrap-vue';
+import BootstrapVue from 'bootstrap-vue'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'babel-polyfill';
